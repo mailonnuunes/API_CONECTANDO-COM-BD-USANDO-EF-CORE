b4714b4970301a433d3665be89ac49231b8ad389
Mailon Azevedo <100615338+mailonnuunes@users.noreply.github.com>
2023-09-10 19:40:50 -0300

    Subindo arquivos

---
+++ APIAlura/Controllers/UsuarioController.cs
@@ -0,0 +1,59 @@
+﻿
+
+using APIAlura.Dto;
+using APIAlura.Entity;
+using APIAlura.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIAlura.Controllers
+{
+    [ApiController]
+    [Route("Usuario")]
+
+   
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+        [HttpGet("obter-todos-com-pedidos/{id}")]
+        public IActionResult ObterTodosComPedidos([FromRoute]int id)
+        {
+            return Ok(_usuarioRepository.ObterComPedidos(id));
+        
+        }
+
+        [HttpGet("Obter-todos-os-usuarios")]
+        public IActionResult ChamarTodosUsuarios()
+        {
+            return Ok(_usuarioRepository.ObterTodos());
+        }
+        [HttpGet("Obter-usuario-por-id/{id}")]
+        public IActionResult ChamarUsuarioID([FromRoute]int id)
+        {
+            return Ok(_usuarioRepository.ObterPorId(id));
+        }
+        [HttpPost]
+        public IActionResult CadastrarUsuario([FromBody]CadastrarUsuarioDto usuarioDto)
+        {
+            _usuarioRepository.Cadastrar(new Usuario(usuarioDto));
+            return Ok("Usuario Cadastrado com sucesso!");
+
+        }
+        [HttpPut]
+        public IActionResult EditarUsuario([FromBody]AlterarUsuarioDto usuarioDto)
+        {
+            _usuarioRepository.Editar(new Usuario(usuarioDto));
+            return Ok("Usuario editado com sucesso!");
+        }
+        [HttpDelete]
+        public IActionResult ExcluirUsuario(int id)
+        {
+            _usuarioRepository.Deletar(id);
+            return Ok("Usuario deletado com sucesso!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# API_CONECTANDO-COM-BD-USANDO-EF-CORE
- API que se conecta ao DB SQL Server usando o Entity Framework Core
+
